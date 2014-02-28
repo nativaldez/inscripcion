aa593ca96f65f3a06681ad6cd3d705182e01b4a9
Natalia Valdez <navtti@gmail.com>
2014-02-28 09:12:43 -0300

    Uso de logger en Curso
    Se agrega un atributo logger a la clase Curso, el test no tiene exito

--- src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
@@ -3,12 +3,20 @@ package org.ticpy.tekoporu.inscripcion;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
 public class Curso {
 
 	private List<String> alumnosMatriculados = new ArrayList<String>();
 
+	@Inject
+	private Logger logger;
+
 	public void matricular(String alumno) {
 		alumnosMatriculados.add(alumno);
+		logger.info("Alumno matriculado con éxito");
 	}
 
 	public boolean estaMatriculado(String alumno) {
