8800a26a9ab96fc3e2728441baed43093546e8d0
Natalia Valdez <navtti@gmail.com>
2014-02-28 10:40:13 -0300

    Nueva clase InscripcionConfig y nuevo archivo inscripcion.properties

--- src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.ticpy.tekoporu.exception.ExceptionHandler;
+import org.ticpy.tekoporu.inscripcion.config.InscripcionConfig;
 import org.ticpy.tekoporu.inscripcion.excepcion.CursoException;
 import org.ticpy.tekoporu.stereotype.Controller;
 import org.ticpy.tekoporu.util.ResourceBundle;
@@ -21,8 +22,11 @@ public class Curso {
 	@Inject
 	private ResourceBundle bundle;
 
+	@Inject
+	private InscripcionConfig config;	
+	
 	public void matricular(String alumno) {
-		if (estaMatriculado(alumno) || alumnosMatriculados.size() == 5) {
+		if (estaMatriculado(alumno) || alumnosMatriculados.size() == config.getCapacidadCurso()) {
 			throw new CursoException();
 		}
 		alumnosMatriculados.add(alumno);
---
+++ src/main/java/org/ticpy/tekoporu/inscripcion/config/InscripcionConfig.java
@@ -0,0 +1,12 @@
+package org.ticpy.tekoporu.inscripcion.config;
+
+import org.ticpy.tekoporu.configuration.Configuration;
+
+@Configuration(resource = "inscripcion")
+public class InscripcionConfig {
+	private int capacidadCurso;
+
+	public int getCapacidadCurso() {
+		return capacidadCurso;
+	}
+}
