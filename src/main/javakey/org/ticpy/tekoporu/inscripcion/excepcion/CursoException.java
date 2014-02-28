348f3566a30fcb0b1db91ec8e18636f081c3300d
Natalia Valdez <navtti@gmail.com>
2014-02-28 11:25:29 -0300

    Utilizacion de anotacion @ApplicationException con rollback=false en CursoException

--- src/main/javakey/org/ticpy/tekoporu/inscripcion/excepcion/CursoException.java
+++ src/main/javakey/org/ticpy/tekoporu/inscripcion/excepcion/CursoException.java
@@ -1,5 +1,8 @@
 package org.ticpy.tekoporu.inscripcion.excepcion;
 
+import org.ticpy.tekoporu.exception.ApplicationException;
+
+@ApplicationException(rollback=false)
 public class CursoException extends RuntimeException{
 
 }
