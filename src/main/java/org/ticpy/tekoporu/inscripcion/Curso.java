2dd6b459498fb8d0e3dede509617c0ac2129aee5
Natalia Valdez <navtti@gmail.com>
2014-02-28 09:26:55 -0300

    Uso de inject en CursoTest
    Modificaciones necesarias para que el test se ejecute con exito

--- src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
-
 import org.slf4j.Logger;
 
 public class Curso {
--- src/test/java/org/ticpy/tekoporu/inscripcion/CursoTest.java
+++ src/test/java/org/ticpy/tekoporu/inscripcion/CursoTest.java
@@ -1,13 +1,20 @@
 package org.ticpy.tekoporu.inscripcion;
 
+import javax.inject.Inject;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ticpy.tekoporu.junit.DemoiselleRunner;
 
+@RunWith(DemoiselleRunner.class)
 public class CursoTest {
-	
+
+	@Inject
+	private Curso curso;
+
 	@Test
 	public void matricularAlumnoConExito() {
-		Curso curso = new Curso();
 		curso.matricular("Andres Gonzalez");
 		Assert.assertTrue(curso.estaMatriculado("Andres Gonzalez"));
 
