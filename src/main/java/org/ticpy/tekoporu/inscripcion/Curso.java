03ed43c26ac60c8385e2a3fb99b47759e23113d6
Natalia Valdez <navtti@gmail.com>
2014-02-28 08:38:47 -0300

    Test de Curso
    Primer test de Curso, clase Curso nueva, implementaciones en CursoTest

---
+++ src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
@@ -0,0 +1,12 @@
+package org.ticpy.tekoporu.inscripcion;
+
+public class Curso {
+	
+	public void matricular(String alumno) {
+	}
+
+	public boolean estaMatriculado(String alumno) {
+		return false;
+	}
+
+}
--- src/test/java/org/ticpy/tekoporu/inscripcion/CursoTest.java
+++ src/test/java/org/ticpy/tekoporu/inscripcion/CursoTest.java
@@ -1,10 +1,16 @@
 package org.ticpy.tekoporu.inscripcion;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CursoTest {
+	
 	@Test
 	public void matricularAlumnoConExito() {
+		Curso curso = new Curso();
+		curso.matricular("Andres Gonzalez");
+		Assert.assertTrue(curso.estaMatriculado("Andres Gonzalez"));
+
 	}
 
 	@Test
