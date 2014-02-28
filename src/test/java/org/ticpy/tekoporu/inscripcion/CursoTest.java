37df850f771bce9a98e6457e70afec334d970b09
Natalia Valdez <navtti@gmail.com>
2014-02-28 08:23:56 -0300

    Test
    Nueva clase de test CursoTest

---
+++ src/test/java/org/ticpy/tekoporu/inscripcion/CursoTest.java
@@ -0,0 +1,17 @@
+package org.ticpy.tekoporu.inscripcion;
+
+import org.junit.Test;
+
+public class CursoTest {
+	@Test
+	public void matricularAlumnoConExito() {
+	}
+
+	@Test
+	public void errorMatricularAlumnoDuplicado() {
+	}
+
+	@Test
+	public void errorMatricularCursoLleno() {
+	}
+}
