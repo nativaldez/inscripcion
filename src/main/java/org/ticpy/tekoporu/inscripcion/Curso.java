bc286c6edb7d255f3a8e6e4edcdc2fc5162da22d
Natalia Valdez <navtti@gmail.com>
2014-02-28 09:02:21 -0300

    Modificaciones en Curso
    Implementaciones realizadas en Curso, el test se ejecuta correctamente

--- src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
+++ src/main/java/org/ticpy/tekoporu/inscripcion/Curso.java
@@ -1,12 +1,18 @@
 package org.ticpy.tekoporu.inscripcion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
-	
+
+	private List<String> alumnosMatriculados = new ArrayList<String>();
+
 	public void matricular(String alumno) {
+		alumnosMatriculados.add(alumno);
 	}
 
 	public boolean estaMatriculado(String alumno) {
-		return false;
+		return alumnosMatriculados.contains(alumno);
 	}
 
 }
