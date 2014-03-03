21884454b018e84e64de6348bbdfc01c133a373d
Natalia Valdez <navtti@gmail.com>
2014-03-03 09:59:16 -0300

    Uso de mesnaje en excepcion

--- src/main/javakey/org/ticpy/tekoporu/inscripcion/excepcion/CursoException.java
+++ src/main/javakey/org/ticpy/tekoporu/inscripcion/excepcion/CursoException.java
@@ -2,7 +2,11 @@ package org.ticpy.tekoporu.inscripcion.excepcion;
 
 import org.ticpy.tekoporu.exception.ApplicationException;
 
-@ApplicationException(rollback=true)
-public class CursoException extends RuntimeException{
+@ApplicationException(rollback = true)
+public class CursoException extends RuntimeException {
+	
+	public CursoException() {
+		super("Error al matricular!");
+	}
 
 }
